72babcc1548a8a2bb3badc9665797493238a37c9
mshayar <studywell5253@gmail.com>
2026-02-28 11:24:05 +0530

    login and registration

--- blog_main/urls.py
+++ blog_main/urls.py
@@ -25,6 +25,9 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.home , name = 'home'),
     path('category/', include('blogs.urls')),
-    path('<slug:slug>/', Blogsviews.single_blog , name='single_blog'),
-    path('blogs/search/' , Blogsviews.search_keyword , name = 'search')
+    path('blog/<slug:slug>/', Blogsviews.single_blog , name='single_blog'),
+    path('blogs/search/' , Blogsviews.search_keyword , name = 'search'),
+    path('register/', views.register , name = 'register'),
+    path('login/', views.login ,name='login'),
+    path('logout/',views.logout , name="logout")
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- blog_main/views.py
+++ blog_main/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 
+from .forms import RegistrationForm
 from blogs.models import Blog, Category
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib import auth
 
 
 def home(request):
@@ -11,3 +14,37 @@ def home(request):
         'post' : post,
     }
     return render (request , 'index.html' , context)
+
+def register(request):
+    if request.method == 'POST' :
+        form = RegistrationForm(request.POST)
+        if form.is_valid:
+            form.save()
+            return redirect ('register')
+    else:
+        form = RegistrationForm()
+    context = {
+        'form' : form ,
+    }
+    return render (request , 'registration.html' , context)
+
+def login(request):
+    if request.method == "POST":
+        form = AuthenticationForm(request , request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+
+            user = auth.authenticate(username=username , password=password)
+            if user is not None:
+                auth.login(request , user)
+            return redirect('home')
+    form = AuthenticationForm()
+    context = {
+        'form' : form 
+    }
+    return render(request , 'login.html', context)
+
+def logout(request):
+    auth.logout(request)
+    return redirect('home')
