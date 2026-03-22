f6b6094dc007352a766d8bb4b7af04b04142163a
mshayar <studywell5253@gmail.com>
2026-03-22 16:52:11 +0530

    category done

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -7,4 +7,8 @@ from blogs import views as Blogsviews
 
 urlpatterns = [
     path('dashboard/',views.dashboard,name='dashboard'),
+    path('dashboard_category/',views.dashboard_category,name='dashboard_category'),
+    path('dashboard_category/dashboard_add_category',views.dashboard_add_category,name='dashboard_add_category'),
+    path('dashboard_category/dashboard_edit_category/<int:pk>/',views.dashboard_edit_category,name='dashboard_edit_category'),
+    path('dashboard_category/dashboard_delete_category/<int:pk>/',views.dashboard_delete_category,name='dashboard_delete_category')
 ]
--- dashboard/views.py
+++ dashboard/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, redirect, render
 from blogs.models import Blog, Category
 from django.contrib.auth.decorators import login_required
+
+from dashboard.forms import CategoryForm
 
 # Create your views here.
 @login_required(login_url='login')
@@ -12,3 +14,41 @@ def dashboard(request):
         'blog_count' : blog_count,
     }
     return render(request , 'dashboard.html', context)
+
+@login_required(login_url='login')
+def dashboard_category(request):
+        return render(request, 'dashboard_category.html')
+
+@login_required(login_url='login')
+def dashboard_add_category(request):
+      if request.method == 'POST':
+            form = CategoryForm(request.POST)
+            if form.is_valid:
+                  form.save()
+                  return redirect('dashboard')
+      form = CategoryForm()
+      context = {
+            'form' : form
+      }
+      return render(request, 'dashboard_add_category.html' , context)
+
+@login_required(login_url='login')
+def dashboard_edit_category(request , pk):
+      category = get_object_or_404(Category, pk=pk)
+      if request.method == 'POST':
+            form = CategoryForm(request.POST ,instance = category)
+            if form.is_valid:
+                  form.save()
+                  return redirect('dashboard_category')
+      form = CategoryForm(instance = category)
+      context = {
+            'form':form,
+            'category' : category,
+      }
+      return render(request, 'dashboard_edit_category.html', context)
+
+@login_required(login_url='login')
+def dashboard_delete_category(request, pk):
+      category = get_object_or_404(Category, pk=pk)
+      category.delete()
+      return redirect('dashboard_category')
